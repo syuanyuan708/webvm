4ab8965830c8a08c03f5e2c1a27216d989023eaf
syuanyuan708 <1667846898@qq.com>
2025-03-03 21:16:51 +0800

    Create triangle.py
    杨辉三角

---
+++ examples/python3/triangle.py
@@ -0,0 +1,19 @@
+def generate_pascals_triangle(n):
+    triangle = [[1] * (i + 1) for i in range(n)]
+    
+    for i in range(2, n):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+    
+    return triangle
+
+def print_pascals_triangle(triangle):
+    max_width = len(" ".join(map(str, triangle[-1])))
+    for row in triangle:
+        row_str = " ".join(map(str, row))
+        print(row_str.center(max_width))
+
+if __name__ == "__main__":
+    n = int(input("Enter the number of rows for Pascal's Triangle: "))
+    triangle = generate_pascals_triangle(n)
+    print_pascals_triangle(triangle)
